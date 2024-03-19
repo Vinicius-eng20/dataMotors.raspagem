12fba0ac56a70f07b0b971b7923d888a4458273d
Vinicius Eduardo Tinoco da Silva <Vinicius.Eduardo@digitronsp.local>
2024-03-19 15:24:00 -0300

    new update

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import pandas as pd
 from carros import Carros
 
-marca = 'Audi'
-modelo = 'A6'
+# marca = 'Audi'
+# modelo = 'A6'
 
 carros = Carros()
-lista = carros.getModelo(marca, modelo)
+# lista = carros.getModelo(marca, modelo)
+lista = carros.getMegaFeirao()
 
-df = pd.DataFrame(lista)
-print(df)
+if len(lista) != 0:
+    df = pd.DataFrame(lista)
+    print(df)
 
-df.to_csv('./dados.csv', encoding='utf-8')
+    df.to_csv('./dados.csv', encoding='utf-8')
+else:
+    print("Lista vazia.")
 
 
 
