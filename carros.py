00b76a3e146bc15eed87aaeb55190deeaa7a4afe
Vinicius Silva <vinisilva5416@gmail.com>
2024-04-30 14:53:38 -0300

    carros modified

--- carros.py
+++ carros.py
@@ -1,11 +1,16 @@
-from requests import get
+from requests import get, post
 import json
-from time import sleep
+from time import sleep, time
 from datetime import date
+from fake_useragent import UserAgent
+import concurrent.futures
+
+ua = UserAgent()
 
 api = 'https://www.webmotors.com.br/api/search/car?'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 OPR/107.0.0.0',
+    #'User-Agent': ua.random,
     'Referer': 'https://www.webmotors.com.br/',
     'Accept': 'application/json, text/plain, */*',
 }
@@ -21,6 +26,8 @@ def json_carro(objeto):
         'cor': objeto['Specification']['Color']['Primary'],
         'transmissao': objeto['Specification']['Transmission'],
         'numPortas': objeto['Specification']['NumberPorts'],
+        'carroceria': objeto['Specification']['BodyType'],
+        'blindado': objeto['Specification']['Armored'],
         'quilometragem': objeto['Specification']['Odometer'],
         'cidade': objeto['Seller']['City'],
         'estado': objeto['Seller']['State'],
@@ -30,59 +37,93 @@ def json_carro(objeto):
         'data': date.today().strftime("%d/%m/%Y")
     }
 
+def insert_API(url, json):
+    response = post(url, json=json)
+    return response
+
+data = {
+    #'url': 'https://www.webmotors.com.br/ofertas/feiroes/megafeirao/carros/estoque?feirao=Mega Feirão',
+    #'feirao': 'Mega Feirão',
+    'url': 'https://www.webmotors.com.br/carros/estoque?lkid=1022',
+    'actualPage': 1,
+    'displayPerPage': 24,
+    'order': 1,
+    'showMenu': True,
+    'showCount': True,
+    'showBreadCrumb': True,
+    'testAB': False,
+    'returnUrl': False
+}
+
 class Carros:
     def getCarsInStock(self):
-        data = {
-            #'url': 'https://www.webmotors.com.br/ofertas/feiroes/megafeirao/carros/estoque?feirao=Mega Feirão',
-            #'feirao': 'Mega Feirão',
-            'url': 'https://www.webmotors.com.br/carros/estoque?lkid=1022',
-            'actualPage': 1,
-            'displayPerPage': 24,
-            'order': 1,
-            'showMenu': True,
-            'showCount': True,
-            'showBreadCrumb': True,
-            'testAB': False,
-            'returnUrl': False
-        }
+        
         response = get(api, headers=headers, params=data)
         response.raise_for_status()
         qtde_items = json.loads(response.text)
         qtde_produtos = qtde_items['Count']
         print(qtde_produtos)
 
-        for j in range(1, 20):
-            data = {
-                #'url': 'https://www.webmotors.com.br/ofertas/feiroes/megafeirao/carros/estoque?feirao=Mega Feirão',
-                #'feirao': 'Mega Feirão',
-                'url': 'https://www.webmotors.com.br/carros/estoque?lkid=1022',
-                'actualPage': j,
-                'displayPerPage': 24,
-                'order': 1,
-                'showMenu': True,
-                'showCount': True,
-                'showBreadCrumb': True,
-                'testAB': False,
-                'returnUrl': False
-            }
+        def paginacao(page):
+            data['actualPage'] = page
             response = get(api, headers=headers, params=data)
 
-            print(response)
-            print(f"Page {j}")
+            print(f"Page {page}: {response}")
 
             if response.status_code != 200:
                 print("Próximo...")
-                continue
+                sleep(1)
+                return
 
             dados = json.loads(response.text)
-
+            carros = []
             for i in range(0, 24):
                 produto = dados['SearchResults'][i]
                 lista_carros.append(json_carro(produto))
+                insert_API("http://localhost:8080/carros", json=json_carro(produto))
+                carros.append(json_carro(produto))
+            
+            sleep(1)
 
-            sleep(.5)
+            return carros
+            
+        # Número total de páginas
+        total_pages = 300
 
-        return lista_carros
+        # Número máximo de threads simultâneas
+        max_workers = 3
+
+        # Lista para armazenar os resultados da raspagem
+        results = []
+
+        # Função para processar as páginas em paralelo
+        def process_pages():
+            with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+                # Mapeia a função scrape_page para cada página
+                future_to_page = {executor.submit(paginacao, page): page for page in range(1, total_pages + 1)}
+                # Coleta os resultados
+                for future in concurrent.futures.as_completed(future_to_page):
+                    page = future_to_page[future]
+                    try:
+                        result = future.result()
+                        results.append(result)
+                        sleep(.5)
+                    except Exception as e:
+                        print(f"Exceção ao processar página {page}: {e}")
+
+        # Inicia o processamento das páginas
+        start_time = time()
+        process_pages()
+        end_time = time()
+
+        # Imprime os resultados
+        print(f"Raspagem concluída em {end_time - start_time:.2f} segundos.")
+        print(f"Total de registros coletados: {len(results)}")
+        
+        # for j in range(1, 50):
+        #     lista_carros.append(paginacao(j))
+
+        # return lista_carros
 
 
 
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # import pandas as pd
 from carros import Carros
-from requests import post
 
 carros = Carros()
 
@@ -8,16 +7,16 @@ lista = carros.getCarsInStock()
 print("Raspagem concluída.")
 
 
-if len(lista) != 0:
-    # df = pd.DataFrame(lista)
-    # print(df)
-    print("Preparando para enviar dados...")
-    # df.to_csv('./dados.csv', encoding='utf-8')
-    for item in lista:
-        response = post(url="http://localhost:8080/carros", json=item)
-        print(response)
-else:
-    print("Lista vazia. ")
+# if len(lista) != 0:
+#     # df = pd.DataFrame(lista)
+#     # print(df)
+#     print("Preparando para enviar dados...")
+#     # df.to_csv('./dados.csv', encoding='utf-8')
+#     for item in lista:
+#         response = post(url="http://localhost:8080/carros", json=item)
+#         print(response)
+# else:
+#     print("Lista vazia. ")
 
 
 
